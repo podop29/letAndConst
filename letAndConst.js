ea05803ca4e848ed6d8eb809589ce813d6b1b5b5
Stevan Grubac <stevangrubac123@gmail.com>
2022-01-17 13:06:30 -0500

    commit

---
+++ letAndConst.js
@@ -0,0 +1,15 @@
+const PI = 3.141
+
+//QUIZ
+
+//1 diffrence between let and var
+//var can be redeclared and has function scope
+
+//2 diffrence between var and const
+//const is block scope, var is function scope, const cant redeclared or redefined var can
+
+//3 diffrence between let and const
+//botha re block scope and cant be redeclared but let can be redefined and const cant
+
+//4what is hoisting
+//When the variable is moved to the top of their function scop before code execution
